79e96b2f1e2391a80bafba46cfd6666bda24d9a7
devlets <leandrovictor2011@gmail.com>
2026-01-02 18:39:36 -0300

    implementação da api dos produtos

--- Pet-love/src/pages/home/index.tsx
+++ Pet-love/src/pages/home/index.tsx
@@ -1,8 +1,28 @@
 import { IoIosArrowForward } from "react-icons/io";
 import img from "../../assets/Pet & Love.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { BsCartPlus } from "react-icons/bs";
+import { api } from "../../services/api";
+
+interface ProductsProps {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
 export function Home() {
-  const [load, setLoad] = useState(true);
+  //const [load, setLoad] = useState(true);
+  const [products, setProducts] = useState<ProductsProps[]>([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get("/products");
+      setProducts(response.data);
+    }
+    getProducts();
+  }, []);
+
   return (
     <div>
       <main className="w-full max-w-7xl mx-auto px-4 text-center ">
@@ -12,10 +32,7 @@ export function Home() {
               Cuidado e <strong className="text-primary">Amor</strong> para seu
               Pet
             </h1>
-            <button
-              onClick={() => setLoad(false)}
-              className="border-none flex gap-2 items-center font-diff font-bold cursor-pointer rounded-3xl  px-3 py-1.5 bg-primary text-white"
-            >
+            <button className="border-none flex gap-2 items-center font-diff font-bold cursor-pointer rounded-3xl  px-3 py-1.5 bg-primary text-white">
               Conhecer Produtos
               <IoIosArrowForward />
             </button>
@@ -33,43 +50,36 @@ export function Home() {
 
         <div
           id="products"
-          className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5 "
+          className="grid gap-4 mb-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-5 "
         >
-          <section className=" p-4">
-            <img
-              src="https://sujeitoprogramador.com/wp-content/uploads/2023/06/racao1.png"
-              alt=""
-              className="w-full rounded-lg max-h-72 mb-2 object-cover"
-            />
-          </section>
-          <section className=" p-4">
-            <img
-              src="https://sujeitoprogramador.com/wp-content/uploads/2023/06/racao1.png"
-              alt=""
-              className="w-full rounded-lg max-h-72 mb-2 object-cover"
-            />
-          </section>
-          <section className=" p-4">
-            <img
-              src="https://sujeitoprogramador.com/wp-content/uploads/2023/06/racao1.png"
-              alt=""
-              className="w-full rounded-lg max-h-72 mb-2 object-cover"
-            />
-          </section>
-          <section className=" p-4">
-            <img
-              src="https://sujeitoprogramador.com/wp-content/uploads/2023/06/racao1.png"
-              alt=""
-              className="w-full rounded-lg max-h-72 mb-2 object-cover"
-            />
-          </section>
-          <section className=" p-4">
-            <img
-              src="https://sujeitoprogramador.com/wp-content/uploads/2023/06/racao1.png"
-              alt=""
-              className="w-full rounded-lg max-h-72 mb-2 object-cover"
-            />
-          </section>
+          {products.map((product) => (
+            <section
+              key={product.id}
+              className="p-1 border border-purpleDark rounded-lg w-full"
+            >
+              <img
+                src={product.cover}
+                alt=""
+                className="w-full rounded-lg max-h-72 object-cover"
+              />
+              <p className="text-white font-medium mt-1 mb-2">
+                {product.title}
+              </p>
+              <div className="flex items-center justify-center gap-3 ">
+                <strong className="text-white text-3xl">
+                  {product.price.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </strong>
+
+                <button className="bg-primaryLight p-1 rounded cursor-pointer">
+                  <BsCartPlus size={20} color="#FFF" />
+                </button>
+              </div>
+              <p className="text-zinc-400">Em até 2x de R$ 122,50 sem juros</p>
+            </section>
+          ))}
         </div>
       </main>
     </div>
